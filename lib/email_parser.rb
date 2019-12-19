69c0db5e3537a1b9278ae733e11d50c568ff29ca
chefkasperson <chefkasperson@gmail.com>
2019-12-18 18:15:14 -0800

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,12 +5,14 @@
 
 class EmailAddressParser
 
-    def initialize(string)
-        @string = string
+    attr_reader :emails
+
+    def initialize(emails)
+        @emails = emails
     end
 
     def parse
-        @string.split(/\, +|\ |\,/).uniq
+        emails.split(/\, +|\ /).uniq
     end
 
 
